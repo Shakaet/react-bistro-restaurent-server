a3524bfabf90c9279981b35b04c15e259520206a
Shakaet <abdshakaet@gmail.com>
2025-01-30 20:24:01 +0600

    add

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const app=express()
 const cors = require('cors');
 const port=process.env.PORT ||5000
 require('dotenv').config()
+const stripe = require('stripe')(process.env.PAYMENT_KEY);
+
 var jwt = require('jsonwebtoken');
 
 app.use(cors())
 app.use(express.json())  
+
+
+  //install stripe
+  
+// npm install --save stripe
 
 // require("crypto").randomBytes(64).toString("hex")
 
@@ -36,9 +43,85 @@ async function run() {
 
     const database = client.db("Bistro-restaurant");
     const menuCollection = database.collection("menu");
-    const reviewCollection = database.collection("menu");
+    const reviewCollection = database.collection("review");
     const cartCollection = database.collection("carts");
     const userCollection = database.collection("users");
+
+
+
+    // payment gateaway
+
+
+    // app.post("/createPaymentIntent",async(req,res)=>{
+
+    //   let {price}=req.body
+    //   let amount=parseInt(price*100)
+    //   // console.log(amount)
+    //   const paymentIntent = await stripe.paymentIntents.create({
+
+    //     amount:amount,
+    //     currency:"usd",
+    //     payment_method_types:["card"]
+        
+    // })
+
+    // res.send({
+    //   clientSecret:paymentIntent.client_secret
+    // })
+
+
+    // })
+
+    //original
+
+    app.post("/createPaymentIntent",async(req,res)=>{
+
+      let {price}=req.body
+      let amount=parseInt(price*100)
+      console.log(amount)
+      const paymentIntent = await stripe.paymentIntents.create({
+
+        amount:amount,
+        currency:"usd",
+        payment_method_types:["card"]
+        
+    })
+
+    res.send({
+      clientSecret:paymentIntent.client_secret
+    })
+  })
+
+
+  // app.post("/createPaymentIntent", async (req, res) => {
+  //   try {
+  //     let { price } = req.body;
+  //     let amount = parseInt(price * 100);
+  
+  //     console.log("Received Price:", price);
+  //     console.log("Calculated Amount:", amount);
+  
+  //     const paymentIntent = await stripe.paymentIntents.create({
+  //       amount: amount,
+  //       currency: "usd",
+  //       payment_method_types: ["card"],
+  //     });
+  
+  //     console.log("Generated Client Secret:", paymentIntent.client_secret);
+  
+  //     res.send({
+  //       clientSecret: paymentIntent.client_secret,
+  //     });
+  
+  //   } catch (error) {
+  //     console.error("Stripe Payment Error:", error);
+  //     res.status(500).send({ error: error.message });
+  //   }
+  // });
+  
+
+
+  
 
    
 
@@ -193,6 +276,64 @@ async function run() {
         
         res.send(result)
     })
+
+
+    app.get("/menu/:id",async(req,res)=>{
+      let id= req.params.id
+      let filter={_id:new ObjectId(id)}
+      let result= await menuCollection.findOne(filter)
+
+      res.send(result)
+    })
+
+
+    app.delete("/menuDelete/:id",verifyToken,verifyAdmin,async(req,res)=>{
+
+      let id=req.params.id
+
+      // let query={_id:new ObjectId(id)}
+       let query={_id:new ObjectId(id)}
+
+      let result=await menuCollection.deleteOne(query)
+      res.send(result)
+    })
+
+
+    app.post("/menu",verifyToken,verifyAdmin,async(req,res)=>{
+      let menuItemData=req.body
+
+      const result = await menuCollection.insertOne(menuItemData);
+
+       res.send(result)
+
+
+    })
+
+    app.put("/menu/:id",async(req,res)=>{
+
+      let id=req.params.id
+      let updateData=req.body
+
+      let filter={_id:new ObjectId(id)}
+
+      const updateDoc = {
+        $set: {
+          name:updateData.name,
+          category:updateData.category,
+          price:updateData.recipe,
+          recipe:updateData.recipe,
+          image:updateData.image
+        },
+      };
+
+      const result = await menuCollection.updateOne(filter, updateDoc);
+
+      res.send(result)
+  
+    })
+
+  
+    
 
     app.get("/reviews",async(req,res)=>{
       let result= await reviewCollection.find().toArray()
